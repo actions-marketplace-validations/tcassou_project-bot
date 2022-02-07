c4115d945db0d464126940da23c41a6b66e1112e
Thomas Cassou <thomas.cassou@pennylane.tech>
2022-02-07 11:56:02 +0100

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from app.github_api import GithubAPI
 ENVIRONMENT = {
     "GITHUB_GRAPHQL_URL": "https://api.foo.com/graphql",
     "GITHUB_API_TOKEN": "secret_token_nobody_should_ever_see",
-    "GITHUB_EVENT_NAME": "issue",
+    "GITHUB_EVENT_NAME": "issues",
     "GITHUB_EVENT_PATH": "/github/workflow/event.json",
     "INPUT_PROJECT_URL": "https://github.com/orgs/foo/projects/123",
 }
